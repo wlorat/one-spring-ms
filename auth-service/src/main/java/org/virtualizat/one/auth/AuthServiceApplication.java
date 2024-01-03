e58271703e7d64ef10e3072805d9d0fb3083bf6f
Willian Jose Lora Torres <wlorat@gmail.com>
2024-01-03 08:16:34 -0500

    add auth-service

--- auth-service/src/main/java/org/virtualizat/one/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/org/virtualizat/one/auth/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package org.virtualizat.one.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AuthServiceApplication {
 
