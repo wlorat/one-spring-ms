fe8681a8b4cd4f213144d17590b69304f59c0487
Willian Jose Lora Torres <wlorat@gmail.com>
2024-01-06 20:45:51 -0500

    mhs-resimag-service

---
+++ mhs-resimag-service/src/main/java/org/medihelp/imagenes/ordenes/resultados/MhsResimagServiceApplication.java
@@ -0,0 +1,13 @@
+package org.medihelp.imagenes.ordenes.resultados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhsResimagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhsResimagServiceApplication.class, args);
+	}
+
+}
